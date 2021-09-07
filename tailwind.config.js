8b0dbd06477ce3717e45aa48bfb6a50be0ac59c9
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-09-07 09:24:41 +0100

    feat: Setup _document

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: ['jit'],
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#0078D4',
+      primaryAlt: '#006BBD',
+      secondary: '#FB923C',
+      secondaryAlt: '#FB8728',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.red,
+    },
+    fontFamily: {
+      sans: ['"Montserrat", sans-serif'],
+    },
   },
   variants: {
     extend: {},
